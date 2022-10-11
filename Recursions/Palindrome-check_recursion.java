01a8039be5d2f26b42a96f816258c55414e550a5
Varun Sharma <62585746+varunsharma6956@users.noreply.github.com>
2022-10-11 16:58:53 +0530

    Update Palindrome-check_recursion.java

--- Recursions/Palindrome-check_recursion.java
+++ Recursions/Palindrome-check_recursion.java
@@ -1,24 +1,28 @@
 import java.io.*;
 import java.util.*;
 
-class Main{
+class Main
+{
+static int rev(int num, int temp)
+{
 
-	
-	static boolean isPalindrome(String str, int start, int end)
-	{
-		if(start >= end)
-			return true;
-
-		return ((str.charAt(start)==str.charAt(end)) && 
-			     isPalindrome(str, start + 1, end - 1));
-	}
-    public static void main(String [] args) 
-    {
-    	
-    	String s = "MelonoeeG";
-
-    	System.out.println(isPalindrome(s, 0, s.length() -1));
-        
-    }
-
+    if (num == 0)
+        return temp;
+ 
+    temp = (temp * 10) + (num % 10);
+ 
+    return rev(num / 10, temp);
+}
+ 
+// Driver Code
+public static void main (String[] args)
+{
+    int num = 121;
+    int temp = rev(num, 0);
+     
+    if (temp == num)
+        System.out.println("yes");
+    else
+        System.out.println("no" );
+}
 }
