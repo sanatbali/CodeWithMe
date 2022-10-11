98506a27df3ab7c15ecf01a5709ed11680680ca5
pranitrathod <pranitrathodsocail@gmail.com>
2022-10-11 17:53:20 +0530

    Reduced the time complixity!

--- Recursions/DigitSum.java
+++ Recursions/DigitSum.java
@@ -3,7 +3,7 @@ package com.pranit.Recursions;
 public class DigitSum {
     public static void main(String args[])
     {
-        int answer= sum(1234);
+        int answer= sum(123456);
         System.out.println(answer);
     }
     static int sum(int no)
